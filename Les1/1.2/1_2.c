fbd6c220247b78f72a62f628a915cb71081579dd
PRoFFiSioNaL <tank.fsb@yandex.ru>
2023-07-18 21:14:43 +0700

    finish_1_2

---
+++ Les1/1.2/1_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void SUCCESS(){
+    printf("Good\n");
+}
+
+int main(int argc, char *argv[]){
+    pid_t pid = fork();
+    int rv;
+    switch(pid){
+        case -1:
+            perror("Fail fork\n");
+            exit(EXIT_FAILURE);
+        case 0:
+            for (int i = 1; i < argc; ++i){
+                printf("%s ", argv[i]);
+            }
+            printf("\n");
+            exit(rv);
+        default:
+            for (int i = 1; i < argc; ++i){
+                printf("%s ", argv[i]);
+            }
+            printf("\n");
+            wait(&rv);
+            printf("%d\n", WEXITSTATUS(rv));
+    }
+    atexit(SUCCESS);
+    exit(EXIT_SUCCESS);
+}
