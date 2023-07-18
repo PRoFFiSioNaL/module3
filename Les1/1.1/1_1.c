bc16a502497e4b51cb692200b6fb291ad6437045
PRoFFiSioNaL <tank.fsb@yandex.ru>
2023-07-18 18:15:15 +0700

    finish_1_1

--- Les1/1.1/1_1.c
+++ Les1/1.1/1_1.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(){
-    
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
+    pid_t uid = getuid();
+    pid_t euid = geteuid();
+    pid_t gid = getgid();
+    pid_t sid = getsid(pid);
+    printf("%d - pid\n%d - ppid\n%d - uid\n%d - euid\n%d - gid\n%d - sid\n", pid, ppid, uid, euid, gid, sid);
+    while(1);
+    exit(EXIT_SUCCESS);
 }
